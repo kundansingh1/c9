dd2de901cb4d0943acad900bb79fb48dd3d44b36
Kundan <kundan.singh979+web@gmail.com>
2016-04-28 01:57:21 +0000

    Finsished working with JSON API

--- httpApi.js
+++ httpApi.js
@@ -1,15 +1,47 @@
 var http = require('http');
-var port = process.argv[2];
-var url  = require('url');
+var url = require('url');
 
-var server = http.createServer(function(req, res) {
-    if(req.url === '/api/parsetime') {
-        var recieveTime = url.query;
-    } else if(req.url === '/api/unixtime') {
-        
-    }
-    
-    res.writeHead(200, {'Content-Type':'application/json'})
-});
+http.createServer(function (req, res) {
+  var urlObj = url.parse(req.url, true),
+      pathname = urlObj.pathname,
+      strtime = urlObj.query.iso,
+      result;
 
-server.listen(port);
+  if (pathname === '/api/unixtime') {
+     result = getUnixTimestamp(strtime);
+  }
+  else if (pathname === '/api/parsetime') {
+    result = getTimeObj(strtime);
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+  }
+  else {
+    res.writeHead(404);
+    res.end();
+  }
+  
+}).listen(process.argv[2]);
+
+
+function getUnixTimestamp(strtime) {
+  return {
+    unixtime: getTimestamp(strtime)
+  };  
+}
+
+function getTimestamp(strtime) {
+  return Date.parse(strtime);
+}
+
+function getTimeObj(strtime) {
+  var date = new Date(getTimestamp(strtime));
+
+  return {
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds()
+  };
+}
